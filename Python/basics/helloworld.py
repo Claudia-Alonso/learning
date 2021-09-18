3b025a7e0215c1744f728b74f4605de01b31a56f
Claudia Alonso <59338779+Claudia-Alonso@users.noreply.github.com>
2021-09-18 23:53:04 +0100

    Create helloworld.py

---
+++ Python/basics/helloworld.py
@@ -0,0 +1,20 @@
+# cmd + shift + p for command palete in VS Code
+# Example file for HelloWorld
+#
+# For internalConsole in launch.json
+def main():
+    print("Hello World!")
+    # name = input("What is you name? ")
+    # print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
+
+# For integratedTerminal in launch.json
+# def main():
+#     print("Hello World!")
+#     name = input("What is you name? ")
+#     print("Nice to meet you,", name)
+
+# if __name__ == "__main__":
+#     main()
