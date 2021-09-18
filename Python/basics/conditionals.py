3f5b0d36b82a17aec194c41f39a03bdec27ad22f
Claudia Alonso <59338779+Claudia-Alonso@users.noreply.github.com>
2021-09-18 23:55:39 +0100

    Create conditionals.py

---
+++ Python/basics/conditionals.py
@@ -0,0 +1,16 @@
+def main():
+    x, y = 100, 100
+
+    # conditional flow uses if, elif, else
+    if (x<y):
+        st = "x is less than y"
+    elif(x == y):
+        st = "x is the same as y"
+    else:
+        st = "x is greater than y"     
+    print (st)
+    # conditional statements let you use "a if C else b"
+    st = "x is less than y" if (x<y) else "x is greater than or the same as y" # Like the ternary operatory in JS
+    print(st)
+if __name__ == "__main__":
+    main()
